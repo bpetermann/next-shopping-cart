974f624b221510688ac0ccc89f8d93ffc883ac49
Benjamin Petermann <benjamin.petermann@gmx.at>
2022-05-27 15:38:05 +0200

    Added product detail description, form and table

--- components/products/ProductDetail.jsx
+++ components/products/ProductDetail.jsx
@@ -1,11 +1,71 @@
 import classes from './ProductDetail.module.css';
 import Image from 'next/image';
+import Accordion from '../ui/Accordion';
+import Footer from '../layout/Footer';
+import { RiTruckLine } from 'react-icons/ri';
+import { BsArrowReturnLeft } from 'react-icons/bs';
+import { GoPackage } from 'react-icons/go';
 
 const ProductDetail = ({ name, description, id, price }) => {
   return (
-    <div className={classes['container']}>
-      <h2>{name}</h2>
-    </div>
+    <>
+      <div className={classes['container']}>
+        <div className={classes['image-container']}>
+          <Image
+            src={`/images/${name}.png`}
+            alt={name}
+            className={classes['product-image']}
+            width={500}
+            height={500}
+          />
+        </div>
+        <div className={classes['product-container']}>
+          <h2>{name}</h2>
+          <p>{description}</p>
+          <p>€{price}</p>
+          <form className={classes['product-order-form']}>
+            <select>
+              <option value='one'>One Size</option>
+            </select>
+            <button>Add to Cart</button>
+          </form>
+          <table className={classes['product-table']}>
+            <tbody>
+              <tr>
+                <td>
+                  <RiTruckLine
+                    size={22}
+                    className={classes['product-table-icons']}
+                  />
+                  Standard Delivery <br />
+                  <p>2-4 business days</p>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <GoPackage
+                    size={22}
+                    className={classes['product-table-icons']}
+                  />
+                  30 days return policy
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <BsArrowReturnLeft
+                    size={22}
+                    className={classes['product-table-icons']}
+                  />
+                  Free Shipping &amp; Returns
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <Accordion />
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 };
 
