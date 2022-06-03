f483043cec26c4a67dfe496a469793992c82f14f
Benjamin Petermann <benjamin.petermann@gmx.at>
2022-06-03 14:34:12 +0200

    Changed model img to next/image

--- components/products/ProductsOverview.jsx
+++ components/products/ProductsOverview.jsx
@@ -1,5 +1,6 @@
 import classes from './ProductsOverview.module.css';
 import Footer from '../layout/Footer';
+import Image from 'next/image';
 import ProductsOverviewItem from './ProductsOverviewItem';
 
 const ProductsOverview = ({ selectedItems }) => {
@@ -11,10 +12,12 @@ const ProductsOverview = ({ selectedItems }) => {
             <h2>DROP 02 SUMMER 2022</h2>
             <p>The 3 Pairs of Shoes You Need for this Summer</p>
           </div>
-          <img
+          <Image
             src='/images/model.png'
             alt='Sneaker Model'
             className={classes['model-image']}
+            width={278}
+            height={425}
           />
         </div>
         <div className={classes['products-container']}>
